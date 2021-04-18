91e949bc79415fb324ddbf14147e03756bf197db
Patryk <patryk.zajac0@gmail.com>
2021-04-18 12:38:41 +0200

    [Init commit]

---
+++ BankAccount/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
---
+++ BankAccountTest/BankAccountTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+using BankAccount;
+
+namespace BankAccountTest
+{
+    public class BankAccountTests
+    {
+        public BankAccountTests()
+        {
+        }
+
+        [Fact]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Credit_WhenAmountIsLessThenZero_ShouldThrowArgumentOutOfRange()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
